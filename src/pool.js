94995fdd02174b524b4c0ea4d09dbdd58e6f1a53
ashraf <ashrafurrahman42069@gmail.com>
2024-03-24 01:28:45 +0600

    Project setup, Create user controller, send response, catch async implemented, auth and user module created

--- src/pool.js
+++ src/pool.js
@@ -2,7 +2,7 @@ const {Pool} = require("pg");
 const config = require("./config");
 
 const pool = new Pool({
-  supabase_url: config.db.supabase_url,
+  connectionString: config.db.supabase_url
 });
 
 module.exports = pool; 
